f979b01ae622c54a8df06cfd6023574fcca2b112
Olivier Gohier <>
2020-04-16 16:39:03 +0200

    auto mount + no configuration needed

--- README.md
+++ README.md
@@ -1,5 +1,28 @@
 # Pinn Os Switcher
+Pinn Os switcher plugin allows you to boot to another Os partition when using PINN Operating System installer.
 
-## Introduction
+## Notes
+* This plugin is ONLY working with KODI installed on Raspberry Pi on an Operating System installed via PINN Operating System installer (like LibreElec).
+* PINN is a version of the NOOBS Operating System Installer for the Raspberry Pi. For more information please see https://github.com/procount/pinn/wiki.
 
-## Version
+## How it works
+Plugin simply bypass PINN by editing the **autoboot.txt** file in the root directory of PINN.
+
+*More specifically:* it mounts the 2 PIN RECOVERY and SETTING partitions (respectively partition 1 and partition 5) to detect te bootable OS available.
+Then, depending on your choice, it adds ```boot_partition=<partition number>``` in **autoboot.txt** file in the root directory of the RECOVERY partition.
+
+**IMPORTANT NOTE:** If you don't want your Raspberry Pi to keep booting to the selected OS afterwards, you need to add a script at the startup of each OS to delete the autoboot.txt file.
+
+## In Work
+* auto remove of autoboot.txt file after reboot
+
+## Versions
+
+**0.2**
+* Auo mount in tmp path (folders are umount and removed afterward)
+* No configuration needed: List of bootable OS is read from PINN SETTING partition ('installed_os.json' file)
+
+**0.1**
+* Init version
+* Moun path is configurable
+* List of bootable OS is manually configurable
--- addon.py
+++ addon.py
@@ -1,53 +1,142 @@
+############################################
+# Pinn Os switcher plugin allows you to boot to another Os partition when using PINN Operating System installer.
+# IMPORTANT: This plugin is ONLY working with KODI installed on Raspberry Pi on an Operating System installed via PINN Operating System installer (like LibreElec).
+# Author: gloug
+#
+# Note: PINN is a version of the NOOBS Operating System Installer for the Raspberry Pi. For more information please see https://github.com/procount/pinn/wiki.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+############################################
+
 import os
 import xbmcaddon
 import xbmcgui
+import json
+from time import strftime
 
-MAX_OS = 5
+__MOUNT_FOLDER__ = "/media"
+__PINN_RECOVERY_PARTITION__ = "/dev/mmcblk0p1"  # name of PINN RECOVERY partition (partition number 1)
+__PINN_RECOVERY_MOUNT_PATH__ = os.path.join(__MOUNT_FOLDER__, "pinn_recovery_%s" % strftime("%Y%m%d_%B%M%S"))  # path where PINN RECOVERY parition will be mounted
+__AUTOBOOT_FILE__ = os.path.join(__PINN_RECOVERY_MOUNT_PATH__, "autoboot.txt")  # path to autoboot.txt file
+__PINN_SETTING_PARTITION__ = "/dev/mmcblk0p5"  # name of PINN SETTING partition (partition number 5)
+__PINN_SETTING_MOUNT_PATH__ = os.path.join(__MOUNT_FOLDER__, "pinn_setting_%s" % strftime("%Y%m%d_%B%M%S"))  # path where PINN SETTING parition will be mounted
+__INSTALLED_OS_JSON__ = os.path.join(__PINN_SETTING_MOUNT_PATH__, "installed_os.json")  # path to installed_os.json file
+__VERIFY_PINN_MSG__ = "Be sure that you are using Kodi on RPI with PINN Operating system installer."
+
+# flags
+__reboot__ = False
+
+def _DEBUG_(*args):
+    xbmcgui.Dialog().ok("DEBUG", *args)
+
+def get_partition_number(bootable_os):
+    # partition must be first element of key "partitions": /dev/mmcblk0pX with X the number
+    return bootable_os["partitions"][0].replace("/dev/mmcblk0p", "")
+
+def clean_close():
+    # umount partitions
+    for mount_dir in os.listdir(__MOUNT_FOLDER__):
+        if mount_dir.startswith("pinn_setting_"):
+            code = os.system("umount %s " %  os.path.join(__MOUNT_FOLDER__, mount_dir))
+            os.rmdir(os.path.join(__MOUNT_FOLDER__, mount_dir))
+
+    for mount_dir in os.listdir(__MOUNT_FOLDER__):
+        if mount_dir.startswith("pinn_recovery_"):
+            code = os.system("umount %s " %  os.path.join(__MOUNT_FOLDER__, mount_dir))
+            os.rmdir(os.path.join(__MOUNT_FOLDER__, mount_dir))
 
 
 addon = xbmcaddon.Addon()
 addonname = addon.getAddonInfo('name')
 
-# get path to pinn system partition
-pinn_sys_part_path = str(xbmcaddon.Addon().getSetting("pinn_system_folder"))
-autoboot_file = os.path.join(pinn_sys_part_path, "autoboot.txt")
+# try mounting Pinn system partitions
+if not os.path.isdir(__PINN_RECOVERY_MOUNT_PATH__):
+    os.makedirs(__PINN_RECOVERY_MOUNT_PATH__)
+if not os.path.isdir(__PINN_SETTING_MOUNT_PATH__):
+    os.makedirs(__PINN_SETTING_MOUNT_PATH__)
+code1 = os.system("mount %s %s" % (__PINN_RECOVERY_PARTITION__, __PINN_RECOVERY_MOUNT_PATH__))
+code2 = os.system("mount %s %s" % (__PINN_SETTING_PARTITION__, __PINN_SETTING_MOUNT_PATH__))
 
-# get all os
+if code1 != 0 or code2 != 0:
+    xbmcgui.Dialog().ok(
+       addonname,
+       "Error mounting PINN partitions",
+       "PINN RECOVERY partition (%s) and/or PINN SETTING partition (%s) cannot be mounted. %s" % (__PINN_RECOVERY_PARTITION__, __PINN_SETTING_PARTITION__, __VERIFY_PINN_MSG__)) 
+    clean_close()
+    exit()
+
+# check installed os
 os_list = []
-for i in range(1, MAX_OS+1):
-    name = str(xbmcaddon.Addon().getSetting("os%s_name" % str(i)))
-    nb_part = str(xbmcaddon.Addon().getSetting("os%s_part_nb" % str(i)))
-    if name != "" and nb_part != "" and nb_part != "0":
-        os_list.append({
-            "name": name,
-            "nb_part": nb_part
-        })
+try:
+    with open(__INSTALLED_OS_JSON__, 'r') as f:
+        os_list = json.loads(f.read())  
+    if type(os_list) != list:  # must be a list of dictionary
+        raise(Exception, "%s is not a list of dict." % __INSTALLED_OS_JSON__)
+except Exception as e:
+    xbmcgui.Dialog().ok(
+        addonname,
+        "Error finding PINN installed OS.",
+        "Failed reading 'installed_os.json' file in PINN SETTING partition (%s)." % __PINN_SETTING_PARTITION__)
+    clean_close()
+    exit()
 
-if len(os_list) > 0:
-    os_idx = xbmcgui.Dialog().select("Choose OS", [x["name"] for x in os_list])
+
+# check bootable OS
+bootable_os = [x for x in os_list if x["bootable"] is True]
+
+if len(bootable_os) > 0:
+    os_idx = int(xbmcgui.Dialog().select("Choose OS", ["%s: %s" % (x["name"], x["description"]) for x in bootable_os]))
 
     # check nb_part > 5 because partition 0 to 5 are reserved to pinn system
-    if int(os_list[os_idx]["nb_part"]) <= 5:
-        xbmcgui.Dialog().ok(addonname, "Error", "Os '%s' has wrong partition configuration, it has to be superior to 5 (partitions 1 to 5 are reserved to pinn system)." % os_list[os_idx]["name"])
-    else:
-        reboot = False
+    if os_idx > -1:
+        try:
+            partition_number = get_partition_number(bootable_os[os_idx])
+        except Exception as e:
+            xbmcgui.Dialog().ok(
+                addonname,
+                "Error getting OS partition number.",
+                "Failed reading '/dev/mmcblk0pX' information (with X number of partition).")
+            clean_close()
+            exit()
 
         try:
-            if os.path.isfile(autoboot_file):
-                os.remove(autoboot_file)
+            # write autoboot.txt
+            if os.path.isfile(__AUTOBOOT_FILE__):
+                os.remove(__AUTOBOOT_FILE__)
 
-            if os.path.isdir(pinn_sys_part_path):
-                with open(autoboot_file, "w") as f:
-                    f.write("boot_partition=%s" % os_list[os_idx]["nb_part"])
-                reboot = True
+            with open(__AUTOBOOT_FILE__, "w") as f:
+                f.write("boot_partition=%s" % partition_number)
+                __reboot__ = True
+
         except Exception as e:
-            xbmcgui.Dialog().ok("Switching to %s" % os_list[os_idx]["name"], "Exception Error", str(e))
-
-        if reboot:
-            xbmcgui.Dialog().notification("Switching to %s" % os_list[os_idx]["name"], "please wait...", time=20000)
-            # import time
-            # with open("d:\\test.txt", "w+") as f:
-            #     f.write(time.strftime("%Y%m%d_%B%M%S"))
-            os.system("reboot")
+            xbmcgui.Dialog().ok(
+                "Switching to %s" % bootable_os[os_idx]["name"],
+                "Exception Error. Please see log file.")
+            clean_close()
+            exit()
 else:
-    xbmcgui.Dialog().ok(addonname, "Error", "No OS configured yet.")
+    xbmcgui.Dialog().ok(
+        addonname,
+        "Error finding bootable OS",
+        "Sorry, there is no bootable OS available. Please verify you PINN configuration.")
+    clean_close()
+    exit()
+
+# reboot
+if __reboot__:
+    xbmcgui.Dialog().notification("Switching to %s" % bootable_os[os_idx]["name"], "please wait...", time=20000)
+    os.system("reboot")
+
+clean_close()
+exit()
